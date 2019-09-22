49944771c417b8ce99cab1665ce0e52d95f362be
Kinyugo <kinyugokyde@gmail.com>
2019-09-22 21:13:48 +0300

    Add GET /todos route

--- server/server.js
+++ server/server.js
@@ -21,6 +21,17 @@ app.post("/todos", (req, res) => {
   todo.save().then(doc => res.send(doc), e => res.status(400).send(e));
 });
 
+app.get("/todos", (req, res) => {
+  Todo.find({}).then(
+    todos => {
+      res.send({ todos });
+    },
+    e => {
+      res.status(400).send(e);
+    }
+  );
+});
+
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
